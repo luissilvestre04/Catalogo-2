67404da0f5c2dca1a66e4ea068fc373d87b063de
luissilvestre04 <tvsilvestree@gmail.com>
2023-11-20 18:59:38 -0300

    final js commit

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+//adicionando filmes 
 function adicionarFilme () {
+  //adicionando valor do filme
   var filmeFavorito =
-  document.getElementById('filme').value 
-   document.write('<img src=' + filmeFavorito +'>')
+  document.getElementById('filme').value
+  //adicionando lista e imagem 
+  var elementoListaFilmes = document.getElementById('listaFilmes')
+  elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>'
+  //cod para limpar barra de url
+  document.getElementById('filme').value='' 
+
+
 }
